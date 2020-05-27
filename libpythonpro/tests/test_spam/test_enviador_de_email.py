21cf69d88f7fc8b6fb4e27ee76e29828a6a07f13
Gabriel <gabrieltggv29@gmail.com>
2020-05-26 22:04:39 -0300

    Implementada a parametrização de testes

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,24 @@
+import pytest
+
+from libpythonpro.spam.enviador_de_email import Enviador
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize(
+    'destinatario',
+    ['foo@bar.com.br', 'gabrieltggv29@gmail.com']
+)
+def test_remetente(destinatario):
+    enviador = Enviador()
+
+    resultado = enviador.enviar(
+        destinatario,
+        'suporte@vilatec.com.br',
+        'Curso Python Pro',
+        'Primeiro teste da turma Luiz Vital.'
+    )
+    assert destinatario in resultado
