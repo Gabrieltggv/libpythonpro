e9558ff50eeee20643f24d28bb736ab8230c7b8a
Gabriel <gabrieltggv29@gmail.com>
2020-06-22 11:46:19 -0300

    Acrescentado o conftest.py
    close #16

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,24 +1,4 @@
-import pytest
-
 from libpythonpro.spam.modelos import Usuario
-from libpythonpro.spam.db import Conexao
-
-
-@pytest.fixture(scope='session')
-def conexao():
-    # Setup
-    conexao_obj = Conexao()
-    yield Conexao()
-    # Tear Down
-    conexao_obj.fechar()
-
-
-@pytest.fixture
-def sessao(conexao):
-    sessao_obj = conexao.gerar_sessao()
-    yield sessao_obj
-    sessao_obj.roll_back()
-    sessao_obj.fechar()
 
 
 def test_salvar_usuario(sessao):
