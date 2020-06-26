4d700bbd3f1a96fba2ef66957bfb5a850e435b36
Gabriel <gabrieltggv29@gmail.com>
2020-06-25 21:02:43 -0300

    Implementado a lib Mock
    close #18

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,15 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='Gabriel')
+    usuario = Usuario(nome='Gabriel', email='gabrieltggv29@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='Gabriel'), Usuario(nome='Leonel')]
+    usuarios = [Usuario(nome='Gabriel', email='gabrieltggv29@gmail.com'),
+                Usuario(nome='Leonel', email='gabrieltggv29@gmail.com')
+                ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
